38f8231e2a8148e258f65c188f4f08593c4dec22
Deepak Mamman <deepakmamman08@gmail.com>
2024-08-28 13:19:05 +0530

    Add functionality to store data in local storage

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const addTodos = () => {
   console.log(inputField.value);
   //console the input field value which written by user.
   let D_div = document.createElement("div");
-  D_div.classList.add = "dynamic_div";
+  D_div.classList.add("dynamic_div");
 
   let pElem = document.createElement("p");
   //now create new paragraph using creat element and pass tag name in "" .
@@ -28,8 +28,25 @@ const addTodos = () => {
   //now append delButton in addTodo div.
 
   addTodoDiv.append(D_div);
+  
+  //calling saveTodo function to save data in local storage
+  saveTodos();
+
   inputField.value = "";
 };
+
+const  saveTodos = () => {
+  const TodoData = [];
+  //create array saveTodo
+  addTodoDiv.querySelectorAll('.dynamic_div').forEach(D_div => {
+    //apply for each loop is used to select all elements with the class dynamic_div inside the addTodoDiv container and perform operations on each of these elements
+
+    const text = D_div.querySelector('p').textContent;
+    TodoData.push(text);
+  })
+  localStorage.setItem("myData",JSON.stringify(TodoData));
+  console.log('data is added')
+}
 
 addButton.addEventListener("click", () => {
   //first add event listner,and call add todos function .
@@ -42,9 +59,11 @@ addTodoDiv.addEventListener("click", (e) => {
 
   if (e.target.tagName === "BUTTON") {
     // apply conditon if the tagName which is targeting equals to button then
-    e.target.previousSibling.remove();
-    // remove previous sibling of that event.means privious sibling of delButton is paragraph
-    e.target.remove();
+    // e.target.previousSibling.remove();
+    // // remove previous sibling of that event.means privious sibling of delButton is paragraph
+    // e.target.remove();
+    e.target.parentElement.remove(); // Remove the parent div
     // romove targeted event means delButton
+    saveTodos();
   }
 });
